df5174f2623791e0ee632b76ce4da2159ed7fc77
Trohat <petr.sed@centrum.cz>
2021-12-11 07:36:13 +0100

    D11 - one function for both tasks

--- D11/index.js
+++ D11/index.js
@@ -15,7 +15,7 @@ const prepare = data => {
     return map;
 }
 
-const task1 = octopuses => {
+const task = (octopuses, task) => {
     const flashOctopus = key => {
         octopuses[key].energy++;
         if (octopuses[key].energy > 9) {
@@ -33,51 +33,22 @@ const task1 = octopuses => {
     }
 
     let flashes = 0;
-    let steps = 100;
-    for (let step = 0; step < steps; step++) {
+    let step = 0;
+    while (true) {
+        step++;
         for (const key of Object.keys(octopuses)) {
             if (!octopuses[key].flashed) {
                 flashOctopus(key);
             }
         }
-        for (const key of Object.keys(octopuses)) 
-            octopuses[key].flashed = false;
-    }
-    return flashes;
-};
 
-const task2 = octopuses => {
-    const flashOctopus = key => {
-        octopuses[key].energy++;
-        if (octopuses[key].energy > 9) {
-            flashes++;
-            octopuses[key].energy = 0;
-            octopuses[key].flashed = true;
-            let [x, y] = key.split(",");
-            for (const dir of diagonalDirs) {
-                newX = +x + dir.x;
-                newY = +y + dir.y;
-                newOct = [newX, newY].toString();
-                if (newOct in octopuses && !octopuses[newOct].flashed) flashOctopus(newOct);
-            }
-        }
-    }
-
-    let flashes = 0;
-    let steps = 1000;
-    for (let step = 1; step <= steps; step++) {
-        for (const key of Object.keys(octopuses)) {
-            if (!octopuses[key].flashed) {
-                flashOctopus(key);
-            }
-        }
-        if (Object.keys(octopuses).every(oct => octopuses[oct].flashed)) return step + 100;
+        if (task === "task2" && Object.keys(octopuses).every(oct => octopuses[oct].flashed)) return step + 100;
         
         for (const key of Object.keys(octopuses)) 
-            octopuses[key].flashed = false;
+        octopuses[key].flashed = false;
+        
+        if (task !== "task2" && step === 100) return flashes;
     }
-    console.log("we dont want to be here");
-    return flashes;
 };
 
 let testdata = `5483143223
@@ -103,15 +74,15 @@ console.log(inputdata);
 
 console.log("");
 
-doEqualTest(task1(testdata), 1656);
+doEqualTest(task(testdata), 1656);
 
 console.time("Task 1");
-console.log("Task 1: " + task1(inputdata));
+console.log("Task 1: " + task(inputdata));
 console.timeEnd("Task 1");
 
 console.log("");
 
-doEqualTest(task2(testdata), 195);
+doEqualTest(task(testdata, "task2"), 195);
 console.time("Task 2");
-console.log("Task 2: " + task2(inputdata));
+console.log("Task 2: " + task(inputdata, "task2"));
 console.timeEnd("Task 2");
