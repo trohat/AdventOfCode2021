2c28b1c392800bbf8f2b505d4049e46963b223eb
Trohat <petr.sed@centrum.cz>
2021-12-23 17:28:45 +0100

    Calculating points structure

--- D23/index.js
+++ D23/index.js
@@ -2,10 +2,8 @@ console.log("AOC 2021 - Day 23: Amphipod");
 
 const prepare = data => {
     if (data === "test") {
-        //return [["B", "A"], ["C", "D"], ["B", "C"], ["D", "A"]];
         return ["21", "34", "23", "41"];
     }
-    //return [["C", "B"], ["B", "C"], ["A", "D"], ["D", "A"]];
     return ["32", "23", "14", "41"];
 };
 
@@ -42,10 +40,19 @@ const task = (initialRooms, roomSize) => {
         return a === "1".repeat(roomSize) && b === "2".repeat(roomSize) && c === "3".repeat(roomSize) && d === "4".repeat(roomSize);
     };
 
+    getPointsValue = pointsObj => {
+        let score = 0;
+        for (const [ type, points ] of Object.entries(pointsObj)) {
+            score += points * scores[type - 1];
+        }
+        return score;
+    }
+
     const setState = (newStates, key, value) => {
         if (key in newStates) {
-            let points = newStates[key];
-            if (value < points) newStates[key] = value;
+            let oldValue = getPointsValue(newStates[key]);
+            let newValue = getPointsValue(value);
+            if (newValue < oldValue) newStates[key] = value;
         }
         else newStates[key] = value;
         return newStates;
@@ -54,16 +61,20 @@ const task = (initialRooms, roomSize) => {
     let hallway = zeros(7).join("");
     let allScores = [];
     let states = {};
-    states[buildStateString(initialRooms, hallway)] = 0;
+    states[buildStateString(initialRooms, hallway)] = { 1: 0, 2: 0, 3: 0, 4: 0 };
+    let steps = 0;
     while (Object.keys(states).length > 0) {
+        steps++;
         let newStates = {};
         for (const [state, points] of Object.entries(states)) {
             let [rooms, hallway] = state.split("_");
             rooms = rooms.split("-");
             if (isFinal(...rooms)) {
-                allScores.push(points);
+                console.log(points);
+                allScores.push(getPointsValue(points));
                 continue;
             }
+            let newPoints;
             hallway.split("").map(Number).forEach((amphipod, hallPosIndex) => {
                 if (amphipod === 0) return;
                 if (canGoInside(rooms[amphipod - 1], amphipod) && isLegal(amphipod, hallPosIndex, hallway)) {
@@ -71,7 +82,9 @@ const task = (initialRooms, roomSize) => {
                     let newRoom = amphipod + rooms[amphipod - 1];
                     newRooms[amphipod - 1] = newRoom;
                     let newHallway = hallway.setCharAt(hallPosIndex, "0");
-                    newStates = setState(newStates, buildStateString(newRooms, newHallway), points + countSteps(amphipod - 1, hallPosIndex, rooms[amphipod - 1]) * scores[amphipod - 1]);
+                    newPoints = {...points};
+                    newPoints[amphipod] += countSteps(amphipod - 1, hallPosIndex, rooms[amphipod - 1]);
+                    newStates = setState(newStates, buildStateString(newRooms, newHallway), newPoints);
                 }
             });
             rooms.forEach((room, rIndex) => {
@@ -85,7 +98,9 @@ const task = (initialRooms, roomSize) => {
                         let newHallway = hallway.setCharAt(hallIndex, amphipod);
                         let newRooms = [...rooms];
                         newRooms[rIndex] = newRoom;
-                        newStates = setState(newStates, buildStateString(newRooms, newHallway), points + countSteps(rIndex, hallIndex, newRoom) * scores[amphipod - 1]);
+                        newPoints = {...points};
+                        newPoints[amphipod] += countSteps(rIndex, hallIndex, newRoom);
+                        newStates = setState(newStates, buildStateString(newRooms, newHallway), newPoints);
                     })
                 }
             })
@@ -100,8 +115,8 @@ const task1 = data => {
 }
 
 const task2 = data => {
-    let insert = [ "44", "32", "21", "13" ];
-    data = data.map((d,i) => d.at(0) + insert[i] + d.at(1));
+    let insert = ["44", "32", "21", "13"];
+    data = data.map((d, i) => d.at(0) + insert[i] + d.at(1));
     return task(data, 4);
 }
 
@@ -117,15 +132,15 @@ console.log(inputdata);
 
 console.log("");
 
-doEqualTest(task1(testdata), 12521);
+//doEqualTest(task1(testdata), 12521);
 
 console.time("Task 1");
-console.log("Task 1: " + task1(inputdata));
+//console.log("Task 1: " + task1(inputdata));
 console.timeEnd("Task 1");
 
 console.log("");
 
-doEqualTest(task2(testdata), 44169);
+//doEqualTest(task2(testdata), 44169);
 console.time("Task 2");
 console.log("Task 2: " + task2(inputdata));
 console.timeEnd("Task 2");
